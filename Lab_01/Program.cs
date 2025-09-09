e09c8a4b21440b8674ce4147125972fb3e350a1d
HalynaB <galja2908@gmail.com>
2025-09-09 22:22:14 +0200

    tasks 2-3

--- Lab_01/Program.cs
+++ Lab_01/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// task1
 app.MapGet("/api/greeting", (string firstName, string lastName) =>
 {
     var fullName = $"{firstName} {lastName}";
@@ -21,6 +22,38 @@ app.MapGet("/api/greeting", (string firstName, string lastName) =>
 
 })
 .WithName("GetGreeting")
+.WithOpenApi();
+
+// task2
+app.MapGet("/api/time", () =>
+{
+    var nowLocal = DateTime.Now;
+
+    var formatted = $"Current time: {nowLocal:HH:mm:ss}\n" +
+                $"Current date: {nowLocal:dd.MM.yyyy}";
+
+    return Results.Text(formatted, "text/plain");
+})
+.WithName("GetCurrentTime")
+.WithOpenApi();
+
+// task 3
+app.MapGet("/api/random", (int? min, int? max) =>
+{
+    int lower = min ?? 0;
+    int upper = max ?? 100;
+
+    if (upper < lower)
+    {
+        return Results.Text("Error: max must be >= min", "text/plain; charset=utf-8");
+    }
+
+    int value = Random.Shared.Next(lower, upper + 1);
+    var formatted = $"Random number: {value}";
+
+    return Results.Text(formatted, "text/plain");
+})
+.WithName("GetRandomNumber")
 .WithOpenApi();
 
 app.Run();
